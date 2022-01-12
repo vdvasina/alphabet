70d68f227657198debf7a1db0a8ce6b0d231ecb7
kdonegina <onegina.ed@mail.ru>
2022-01-12 11:40:43 +0300

    b ready

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ def a(buf):
     buf[1]=buf[1]+"█▄▄█  "
     buf[2]=buf[2]+"█  █  "
     buf[3]=buf[3]+"      "
+
+def b (buf):
+    buf[0] = buf[0] + "█▀▀█  "
+    buf[1] = buf[1] + "█▀▀▄  "
+    buf[2] = buf[2] + "█▄▄█  "
+    buf[3] = buf[3] + "      "
 s=["","","","",""]
 a(s)
 print(*s,sep="\n")
